7681959a27f89abd5f60e7577cd8489085be5b74
cncsmonster <cncsmonster@163.com>
2023-06-30 23:10:48 +0800

    impl for 3.4

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 
 docker build -t 10.249.12.83:5000/compilerhit/sysy-rv64-cpci:3.4 .
-# docker run --name ci -d -p 50051:50051 -v ./data:/test/data 10.249.12.83:5000/compilerhit/sysy-rv64-cpci:3.3
+# docker run --name ci -d -p 50051:50051 -v ./data:/test/data 10.249.12.83:5000/compilerhit/sysy-rv64-cpci:3.4
